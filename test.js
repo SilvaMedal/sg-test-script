0e1df0f45d44ea11f5caca6ca87b459b2313fcb5
Anthony Silva <budsilva04@gmail.com>
2024-07-29 16:12:49 -0400

    test

---
+++ test.js
@@ -0,0 +1,7 @@
+(function () {
+  console.log("Simple test script loaded");
+
+  document.addEventListener("DOMContentLoaded", function () {
+    console.log("Simple test DOMContentLoaded event fired.");
+  });
+})();
