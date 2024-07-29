63e69a2dd2d42f74c3cf1c2ae8735d7948558df2
Anthony Silva <budsilva04@gmail.com>
2024-07-29 12:44:37 -0400

    revert

--- review-script.js
+++ review-script.js
@@ -13,9 +13,6 @@ function fetchAndUpdateReviewCount() {
       });
     })
     .catch((error) => console.error("Error fetching review count:", error));
-  console.log("In the review script");
 }
-console.log("In the review script");
-fetchAndUpdateReviewCount();
 // Fetch the review count when the page loads
-// document.addEventListener("DOMContentLoaded", fetchAndUpdateReviewCount);
+document.addEventListener("DOMContentLoaded", fetchAndUpdateReviewCount);
