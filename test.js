6a33b023eaffed3dbd566c4da2863d8dcb3ed082
Anthony Silva <budsilva04@gmail.com>
2024-07-29 16:27:26 -0400

    edits

--- test.js
+++ test.js
@@ -1,7 +1,21 @@
-(function () {
-  console.log("Simple test script loaded");
+(function fetchAndUpdateReviewCount() {
+  // Replace 'YOUR_PLACE_ID' with the actual PlaceID
+  var placeId = document
+    .getElementById("masterScript")
+    .getAttribute("review-place-id");
+  var apiUrl = `https://review-update-automation-3bbb074e12a3.herokuapp.com/get-review-count/${placeId}`;
 
-  document.addEventListener("DOMContentLoaded", function () {
-    console.log("Simple test DOMContentLoaded event fired.");
-  });
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      var reviewCount = data.review_count + "+";
+      var reviewElements = document.querySelectorAll(".review-count");
+      reviewElements.forEach(function (elem) {
+        elem.innerText = reviewCount;
+      });
+    })
+    .catch((error) => console.error("Error fetching review count:", error));
+  // Fetch the review count when the page loads
+  //   document.addEventListener("DOMContentLoaded", fetchAndUpdateReviewCount);
+  //   fetchAndUpdateReviewCount();
 })();
