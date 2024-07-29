3c2e0738754b2b6a6017b233c7409671093c8278
Anthony Silva <budsilva04@gmail.com>
2024-07-29 15:49:56 -0400

    edit

--- all-scripts.js
+++ all-scripts.js
@@ -1,38 +1,38 @@
-window.addEventListener("load", function () {
-  // First script's logic here
-  var spans = document.getElementsByTagName("span");
-  for (var i = 0; i < spans.length; i++) {
-    if (spans[i].textContent.includes("©")) {
-      var existingText = spans[i].textContent;
-      var yearPosition = existingText.indexOf("©") + 2;
-      var restOfText = existingText.substring(yearPosition + 4);
+// window.addEventListener("load", function () {
+//   // First script's logic here
+//   var spans = document.getElementsByTagName("span");
+//   for (var i = 0; i < spans.length; i++) {
+//     if (spans[i].textContent.includes("©")) {
+//       var existingText = spans[i].textContent;
+//       var yearPosition = existingText.indexOf("©") + 2;
+//       var restOfText = existingText.substring(yearPosition + 4);
 
-      var now = new Date();
-      var currentYear = now.getFullYear();
+//       var now = new Date();
+//       var currentYear = now.getFullYear();
 
-      spans[i].textContent = "© " + currentYear + restOfText;
-    }
-  }
+//       spans[i].textContent = "© " + currentYear + restOfText;
+//     }
+//   }
 
-  // Second script's logic here
-  for (var i = 0; i < spans.length; i++) {
-    if (spans[i].textContent.includes("EXP.")) {
-      var now = new Date();
-      var nextMonth = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+//   // Second script's logic here
+//   for (var i = 0; i < spans.length; i++) {
+//     if (spans[i].textContent.includes("EXP.")) {
+//       var now = new Date();
+//       var nextMonth = new Date(now.getFullYear(), now.getMonth() + 1, 1);
 
-      var newDate =
-        "EXP. " +
-        String(nextMonth.getMonth() + 1).padStart(2, "0") +
-        "-" +
-        String(nextMonth.getDate()).padStart(2, "0") +
-        "-" +
-        String(nextMonth.getFullYear()).slice(-2);
+//       var newDate =
+//         "EXP. " +
+//         String(nextMonth.getMonth() + 1).padStart(2, "0") +
+//         "-" +
+//         String(nextMonth.getDate()).padStart(2, "0") +
+//         "-" +
+//         String(nextMonth.getFullYear()).slice(-2);
 
-      spans[i].textContent = newDate;
-    }
-  }
-  console.log("Copyright Script ran");
-});
+//       spans[i].textContent = newDate;
+//     }
+//   }
+//   console.log("Copyright Script ran");
+// });
 
 (function fetchAndUpdateReviewCount() {
   // Replace 'YOUR_PLACE_ID' with the actual PlaceID
