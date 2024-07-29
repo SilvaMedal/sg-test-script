b91c318cac19fa3a7d818261205ee58ea662247a
Anthony Silva <budsilva04@gmail.com>
2024-07-29 16:27:33 -0400

    edits

--- all-scripts.js
+++ all-scripts.js
@@ -183,11 +183,3 @@
 //       }
 //     });
 //   })();
-
-(function () {
-  console.log("Simple test script loaded");
-
-  document.addEventListener("DOMContentLoaded", function () {
-    console.log("Simple test DOMContentLoaded event fired.");
-  });
-})();
