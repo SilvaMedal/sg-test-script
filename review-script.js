9af8a6d2a9926ce28f3e1549c2bacc892de8500f
Anthony Silva <budsilva04@gmail.com>
2024-07-29 11:02:11 -0400

    Add review script

---
+++ review-script.js
@@ -0,0 +1,19 @@
+function fetchAndUpdateReviewCount() {
+  // Replace 'YOUR_PLACE_ID' with the actual PlaceID
+  var apiUrl =
+    "https://review-update-automation-3bbb074e12a3.herokuapp.com/get-review-count/ChIJKU52QTV1K4cRXFVN9uo9Rg0";
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      var reviewCount = data.review_count + "+";
+      var reviewElements = document.querySelectorAll(".review-count");
+      reviewElements.forEach(function (elem) {
+        elem.innerText = reviewCount;
+      });
+    })
+    .catch((error) => console.error("Error fetching review count:", error));
+}
+
+// Fetch the review count when the page loads
+document.addEventListener("DOMContentLoaded", fetchAndUpdateReviewCount);
