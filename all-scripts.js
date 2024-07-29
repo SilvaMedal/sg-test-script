7ec98fb9a1d46cfa37b56b49bac7e8bfe9450bd0
Anthony Silva <budsilva04@gmail.com>
2024-07-29 15:28:36 -0400

    update

--- all-scripts.js
+++ all-scripts.js
@@ -32,26 +32,26 @@ window.addEventListener("load", function () {
     }
   }
   console.log("Copyright Script ran");
+
+  (function fetchAndUpdateReviewCount() {
+    // Replace 'YOUR_PLACE_ID' with the actual PlaceID
+    var placeId = document
+      .getElementById("masterScript")
+      .getAttribute("review-place-id");
+    var apiUrl = `https://review-update-automation-3bbb074e12a3.herokuapp.com/get-review-count/${placeId}`;
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        var reviewCount = data.review_count + "+";
+        var reviewElements = document.querySelectorAll(".review-count");
+        reviewElements.forEach(function (elem) {
+          elem.innerText = reviewCount;
+        });
+      })
+      .catch((error) => console.error("Error fetching review count:", error));
+    // Fetch the review count when the page loads
+    //   document.addEventListener("DOMContentLoaded", fetchAndUpdateReviewCount);
+    //   fetchAndUpdateReviewCount();
+  })();
 });
-
-(function fetchAndUpdateReviewCount() {
-  // Replace 'YOUR_PLACE_ID' with the actual PlaceID
-  var placeId = document
-    .getElementById("masterScript")
-    .getAttribute("review-place-id");
-  var apiUrl = `https://review-update-automation-3bbb074e12a3.herokuapp.com/get-review-count/${placeId}`;
-
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      var reviewCount = data.review_count + "+";
-      var reviewElements = document.querySelectorAll(".review-count");
-      reviewElements.forEach(function (elem) {
-        elem.innerText = reviewCount;
-      });
-    })
-    .catch((error) => console.error("Error fetching review count:", error));
-  // Fetch the review count when the page loads
-  //   document.addEventListener("DOMContentLoaded", fetchAndUpdateReviewCount);
-  //   fetchAndUpdateReviewCount();
-})();
